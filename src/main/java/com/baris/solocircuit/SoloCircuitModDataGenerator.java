98a9eb6899865a9230786efe7c67321fc5e9942d
Baris <2501920571@qq.com>
2025-03-05 14:08:27 +0800

    初始化

---
+++ src/main/java/com/baris/solocircuit/SoloCircuitModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.baris.solocircuit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoloCircuitModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
